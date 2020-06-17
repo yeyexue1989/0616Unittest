8acc3e377b30f176cad826748dd20d499150d81f
yeyexue1989 <59593253+yeyexue1989@users.noreply.github.com>
2020-06-17 15:17:19 +0800

    新增应急预案、编辑预案

---
+++ runAll.py
@@ -0,0 +1,15 @@
+import unittest
+import HtmlTestRunner
+from InsuranceManage.EmergencyManageNew import EmergencyManageNew
+from InsuranceManage.EmergencyManageExamine import EmergencyManageExmine
+
+if __name__ == '__main__':
+
+    suite = unittest.TestSuite()
+    #应急预案新建
+    suite.addTest(EmergencyManageNew("test_emergency_manage_new"))
+    #应急预案编辑
+    suite.addTest(EmergencyManageExmine("test_emergency_manage_exmine"))
+    with open('report.html','w') as f:
+        runner = HtmlTestRunner.HTMLTestRunner(stream = f,verbosity=2)
+        runner.run(suite)
